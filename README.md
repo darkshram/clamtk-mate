81e83dbe440bf936bcb65641075f98bf9c6dde90
Joel Barrios <darkshram@gmail.com>
2017-09-24 16:49:01 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# clamtk-mate
+
+This readme file was last updated 7 Sept 2017
+
+# README for clamtk-mate
+
+clamtk-mate is a simple plugin for
+[ClamTk](https://github.com/dave-theunsub/clamtk) to allow a right-click,
+context menu scan of files or folders in the Caja file manager.
+
+It's based on clamtk-gnome https://github.com/dave-theunsub/clamtk-gnome
+
+
+
+### Manual installation
+
+1. Install [ClamTk](https://github.com/dave-theunsub/clamtk). You'll need version 4.00 or newer.
+2. Additionally, you need to install python-caja
+3. Download the project files
+4. As root run `cp clamtk-mate.py /usr/share/python-caja/extensions/`
+5. Restart Caja
+
+
+## Usage and screenshots
+
+1. Bring up the context menu either by
+   1. right clicking on a directory:
+   
+   ![starting scan alt 1](_img/starting_scan_alt1.png)
+   
+   2. Or by clicking here:
+   
+   ![starting scan alt 2](_img/starting_scan_alt2.png)
+
+2. Click on the scan button
+
+3. Wait for the scan to complete (pleate note that it may take a couple of seconds for the scan to start and also for the "No threats found" message to be shown at the end - even after the message "Scanning completed" has been shown)
+![scan result](_img/scan_result.png)
+
+
+## Dependencies
+
+* `clamtk` >= 4.00
+* python-caja
+
+## Links
+
+* http://dave-theunsub.github.io/clamtk/
+* https://github.com/dave-theunsub/clamtk-gnome
+* https://bitbucket.org/davem_/clamtk-gnome
+* http://standards.freedesktop.org/desktop-entry-spec/latest/
+* http://code.google.com/p/clamtk/ (no longer used)
+
+## Contact
+
+* Joel Barrios, darkshram at gmail
---
+++ clamtk-mate.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+#
+# ClamTk, copyright (C) 2004-2017 Dave M
+#
+# This file is part of ClamTk (http://code.google.com/p/clamtk/).
+#
+# ClamTk is free software; you can redistribute it and/or modify it
+# under the terms of either:
+#
+# a) the GNU General Public License as published by the Free Software
+# Foundation; either version 1, or (at your option) any later version, or
+#
+# b) the "Artistic License".
+
+import os
+import pipes
+
+import locale
+locale.setlocale(locale.LC_ALL, '')
+
+import gettext
+_ = lambda x: gettext.ldgettext("clamtk-mate", x)
+
+from gi.repository import Caja, GObject
+
+
+class OpenTerminalExtension(GObject.GObject, Caja.MenuProvider):
+    def __init__(self):
+        print("Initializing clamtk-mate")
+
+    def _open_scanner(self, file):
+        filename = file.get_location().get_path()
+        #- file is of type caja-vsf-file
+        # https://github.com/mate-desktop/caja/blob/master/src/caja-file.h
+        # which inherits from caja-file
+        # https://github.com/mate-desktop/caja/blob/master/src/caja-vfs-file.h
+        #- get_location returns a GFile
+        # https://developer.gnome.org/gio/stable/GFile.html
+        # which has the get_path function which returns the absolute path as a string
+        filename = pipes.quote(filename)
+        # - when switching to Python 3 we can use shlex.quote() instead
+
+        os.system('clamtk %s &' % filename)
+
+    def menu_activate_cb(self, menu, file):
+        self._open_scanner(file)
+
+    def menu_background_activate_cb(self, menu, file): 
+        self._open_scanner(file)
+
+    def get_file_items(self, window, files):
+        if len(files) != 1:
+            return
+        file = files[0]
+
+        item = Caja.MenuItem(
+            name='CajaPython::openscanner',
+            label=_('Scan for threats...') ,
+            tip=_('Scan %s for threats...') % file.get_name(),
+            icon='clamtk')
+        # - the tooltips are not shown any longer in Caja
+        # (the code is kept here in case this changes again for Caja
+        item.connect('activate', self.menu_activate_cb, file)
+
+        return [item]
+
+    def get_background_items(self, window, file):
+        item = Caja.MenuItem(
+            name='CajaPython::openscanner_directory',
+            label=_('Scan directory for threats...'),
+            tip=_('Scan this directory for threats...'),
+            icon='clamtk')
+        # - the tooltips are not shown any longer in Caja
+        # (the code is kept here in case this changes again for Caja
+        item.connect('activate', self.menu_background_activate_cb, file)
+
+        return [item]
